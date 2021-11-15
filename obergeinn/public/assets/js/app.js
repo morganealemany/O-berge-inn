cffbebd73f2934480299a1c496632bec62185f19
morganealemany <m.plancheron@gmail.com>
2021-11-15 11:25:20 +0100

    Addition of current route name condition to execute the map of the event

--- obergeinn/public/assets/js/app.js
+++ obergeinn/public/assets/js/app.js
@@ -75,45 +75,49 @@
      */
     displayMapForEvent: function()
     {
-        // First we will transfom the adress of the event into coordinates
-        // ========= MapBox Search API ==============
-        // Select the event adress from the DOM
-        const address = document.querySelector('#adress').innerHTML;
-
-        // Format the adress to use it in the API
-        const location = address.replaceAll(' ', '%20');
-
-        const apiSearchUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + location + '.json?access_token=pk.eyJ1IjoibW9yZ2FuZTY2IiwiYSI6ImNrdjBzbTh6eDBrbjMyb2xwdzRyNnVpZnoifQ.oU_h1HqLy6f1-3o58Qkerg';
-        
-        const searchConfig = {
-            method: 'GET',
-            mode: 'cors',
-            cache: 'no-cache'
-        };
-
-        fetch(apiSearchUrl, searchConfig)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(fullCoordinates) {
-
-            const destinationCoordinates = fullCoordinates.features[0].center;
-            // Then we have to create a map
-            const map = L.map('map').setView([destinationCoordinates[1],destinationCoordinates[0]], 12);
-            L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-                maxZoom: 20
-            }).addTo(map);
-
-            // And a marker for the adress of the event
-            const marker = L.marker([destinationCoordinates[1],destinationCoordinates[0]]).addTo(map);
+        // If the current page is the details of an event
+        if ((window.location.pathname).search("evenement/details") === 1 ) {
+            // First we will transfom the adress of the event into coordinates
+            // ========= MapBox Search API ==============
+            // Select the event adress from the DOM
+            const address = document.querySelector('#adress').innerHTML;
             
-            // Then use the current position coordinates transfered in parameters and link the itinerary from the current position and the destination
-            marker.bindPopup('<a target="_blank" href="https://www.google.com/maps/search/' + destinationCoordinates[1] + ',' + destinationCoordinates[0] + '" ><h3>Y aller</h3></a>');
-
-            // TODO For directions with a geolocation
-            // marker.bindPopup('<a target="_blank" href="https://www.google.com/maps/dir/' + latitude + ','  + longitude + '/' + destinationCoordinates[1] + ',' + destinationCoordinates[0] + '" ><h3>Y aller</h3></a>');
-        })
-    },
-}
-
-document.addEventListener('DOMContentLoaded', app.init);
+            // Format the adress to use it in the API
+            const location = address.replaceAll(' ', '%20');
+            
+            const apiSearchUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + location + '.json?access_token=pk.eyJ1IjoibW9yZ2FuZTY2IiwiYSI6ImNrdjBzbTh6eDBrbjMyb2xwdzRyNnVpZnoifQ.oU_h1HqLy6f1-3o58Qkerg';
+            
+            const searchConfig = {
+                method: 'GET',
+                mode: 'cors',
+                cache: 'no-cache'
+            };
+            
+            fetch(apiSearchUrl, searchConfig)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(fullCoordinates) {
+                
+                const destinationCoordinates = fullCoordinates.features[0].center;
+                // Then we have to create a map
+                const map = L.map('map').setView([destinationCoordinates[1],destinationCoordinates[0]], 12);
+                L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+                    maxZoom: 20
+                }).addTo(map);
+                
+                // And a marker for the adress of the event
+                const marker = L.marker([destinationCoordinates[1],destinationCoordinates[0]]).addTo(map);
+                
+                // Then use the current position coordinates transfered in parameters and link the itinerary from the current position and the destination
+                marker.bindPopup('<a target="_blank" href="https://www.google.com/maps/search/' + destinationCoordinates[1] + ',' + destinationCoordinates[0] + '" ><h3>Y aller</h3></a>');
+                
+                // TODO For directions with a geolocation
+                // marker.bindPopup('<a target="_blank" href="https://www.google.com/maps/dir/' + latitude + ','  + longitude + '/' + destinationCoordinates[1] + ',' + destinationCoordinates[0] + '" ><h3>Y aller</h3></a>');
+            })
+        }
+        },
+    }
+    
+    document.addEventListener('DOMContentLoaded', app.init);
+    
